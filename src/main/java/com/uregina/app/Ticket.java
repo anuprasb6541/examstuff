d2df1f76ac3172c21a391ebf6f8edf8cf593dde0
Anupras Burokas <anuprasb750@gmail.com>
2021-04-27 16:18:31 -0600

    Pushed my files

---
+++ src/main/java/com/uregina/app/Ticket.java
@@ -0,0 +1,101 @@
+package com.uregina.app;
+
+import com.uregina.exceptions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+/**
+ * Class Ticket, a List of Flights in ordered sequences
+ *
+ */
+public class Ticket
+{
+	
+	private static final String [] SchengenAirportsCode = new String[] { "VIE","BRU","PRG","CPH","TLL","HEL","CDG","FRA","MUC","ATH","BUD","KEF","CIA","RIX","VNO","LUX","MLA","AMS","OSL","WAW","LIS","LJU","KSC","MAD","ARN","BRN"};
+	/**
+	 * The function checks the validaity of a ticket
+	 * It checks
+	 *		1. airports code are in IATA format (any three uppercases letters)
+	 *		2. maximum flights Count >= number of flights in the ticket
+	 *		3. maximum flight time >= Total flight times ( sum of flight time of each flight)
+	 *		4. maximum layover time >= Total layover times ( sum of layover time between each consequative flight)
+	 *		5. no flight between two airports in the Schengen area unless the passenger has a valid SchengenVisa
+	 *		6. no cyclic trip
+	 *		7. The sequence of flights in correct ( the arrival airport of a flight is the departure airport of the next flight)
+	 * 		8. any other logical constraints 
+	 * @param	ticket				List of ordered sequences of flights
+	 * @param	maxFlightsCount		Maximum number of flights in the ticket
+	 * @param	maxFlightTime		Maximum allowable total flight times in minutes
+	 * @param	maxLayoverTime		Maximum allowable total layover times in minutes
+	 * @param	hasSchengenVisa		to indicate if the passenger has a valid Schengen Visa or not
+	 * @return	true if the ticket is valid, false otherwise
+	 * usefeul functions that you may use
+	 * 		(class: Flight , method: getArrivalAirport)
+	 * 		(class: Flight , method: getDepatureAirport)
+	 * 		(class: Flight , method: calculateFlightTime)
+	 * 		(class: Flight , method: calculateLayoverTime)
+	 * 		(class: ticket , method: hasCyclicTrip)
+	*/
+	public static boolean checkTicket( ArrayList<Flight> ticket, int maxFlightsCount, int maxFlightTime, int maxLayoverTime, boolean hasSchengenVisa)
+	{
+		//Todo: add your code here
+		// not sure how to test if in IATA format yet... maybe will figure out maybe not...
+		
+		// condition 2: max flight count >= num of flights
+		if (ticket.size() > maxFlightsCount) return false;
+
+		// condition 3: max flight time >= total flight times combined
+		int flightTime = 0;
+		for (int i=0; i < ticket.size(); i++){
+			flightTime += ticket.get(i).calculateFlightTime();
+		}
+
+		if (flightTime > maxFlightTime) return false;
+
+		//condition 4: max layover time >= total layover times combined
+		int layoverTime = 0;
+		for (int i=0; i < ticket.size() - 1; i++){
+			layoverTime += Flight.calculateLayoverTime(ticket.get(i), ticket.get(i+1));
+		}
+
+		if (layoverTime > maxLayoverTime) return false;
+
+		//condition 5: no flight between two airports in the Schengen area unless the passenger has a valid SchengenVisa
+		int schengenAirports = 0;
+		for (Flight flight : ticket){
+			for (int i=0; i < Ticket.SchengenAirportsCode.length; i++){
+				if (flight.getArrivalAirport().equals(Ticket.SchengenAirportsCode[i]){
+					schengenAirports++;
+				}
+			}
+		}
+
+		if ((schengenAirports > 1) && (!hasSchengenVisa)) return false;
+
+		//condition 6: no cyclic trip, has not been implemented
+
+		//condition 7: The sequence of flights in correct ( the arrival airport of a flight is the departure airport of the next flight)
+		for(int i = 0; i < ticket.size() - 1; i++){
+			if (!(ticket.get(i).getArrivalAirport().equals(ticket.get(i+1).getDepatureAirport()))) return false;
+		}
+
+		//end of your code
+		return true;
+	}
+		/**
+	 * The function checks if the ticket has a cyclic trip
+	 * (that is, no passenger can arrive at the same airport more than once within the same ticket)
+	 * @param	ticket				List of ordered sequences of flights
+	 * @return	true if the ticket has a cyclic trip, false otherwise
+	 * usefeul functions that you may use
+	 * 		(class: Flight , method: getArrivalAirport)
+	 * 		(class: Flight , method: getDepatureAirport)
+	*/
+	public static boolean hasCyclicTrip(ArrayList<Flight> ticket)
+	{
+		//Todo : add your code here
+		
+
+		//Todo : end of your code
+		return false;
+	}
+}
---
+++ src/main/java/com/uregina/app/Time24.java
@@ -0,0 +1,94 @@
+package com.uregina.app;
+
+import com.uregina.exceptions.*;
+
+/**
+ * 24 hour time
+ * 
+ */
+public class Time24 
+{
+	int hours;   // 0:23
+	int minutes; // 0:59
+    /** 
+	 * constructor to initial Time24 object
+	 * @param hours 		hours of 24-hour time format (0:23)
+	 * @param minutes	minutes of 24-hour time format (0:59)
+	*/
+	public Time24(int hours, int minutes) throws InvalidTimeException
+    {
+		if(hours<0||hours>=24) throw new InvalidTimeException();
+		if(minutes<0||minutes>=60) throw new InvalidTimeException();
+		this.hours=hours;
+		this.minutes=minutes;
+    }
+	/**
+	* returns the hours of 24-hour time format (0:23)
+	*/
+	public int getHours(){
+		return this.hours;
+	}
+	/**
+	* returns the minutes of 24-hour time format (0:59)
+	*/
+	public int getMinutes(){
+		return this.minutes;
+	}
+	/**
+	 * Convert time 12-hour to 24-hour
+	 * @param  houra		hours in 12-hour time format (1:12)
+	 * @param  minutea	minutes in 24-hour time format (0:59)
+	 * @param  am_pm	enumerator with the value of am or pm
+	 * @return 		An equavalent Time24 object
+	 * 				a null for invalid input parameters
+	 * 				No exception should be thrown
+	 * @see 	https://www.freecodecamp.org/news/mathematics-converting-am-pm-to-24-hour-clock/
+	 * @note	(12 am and 12 pm are special cases)
+	 */
+	public static Time24 toTime24(int hours, int minutes, AmPm am_pm)
+	{
+		Time24 time=null;
+		//Todo : add your code here
+		// case 1: if time is between 12:00 AM and 12:59 AM, then we subtract 12 hours, so effectively making the hour = 0
+		if (hours == 12 && (minutes >= 0) && (minutes <=59) && (am_pm == am)){
+			//hours = 0;
+			time.hours = hours - 12;
+		}
+		// case 2: if the time is between 1:00 AM and 12:59 PM, the hours remain the same, so don't change anything
+		// case 3: if the time is between 1:00 PM and 11:59 PM, then we'd add 12 hours
+		if ((hours >= 1) && (hours <= 11) && (minutes >= 0) && (minutes <= 59) && (am_pm == pm)){
+			time.hours = hours + 12;
+		}
+
+		// special cases:
+		// case 12 am is accounted for in case 1 above
+		// case 12 pm is accounted for in case 2 above
+		
+
+
+		// End of your code
+		return time;
+	}
+	/**
+	 * subtract two times
+	 * @param  t1	integer from 1:12
+	 * @param  t2	integer fro 0:59
+	 * @return 		the value of (t1-t2) in minutes
+	 * 				it may be a negative value
+	 */
+	public static int subtract(Time24 t1, Time24 t2)
+	{
+		int dHour=t1.hours-t2.hours;
+		int dMinute=t1.minutes-t2.minutes;
+		return dHour*60+dMinute;
+	}
+	/**
+	 * Convert Time24 object to string
+	 * @param  None
+	 * @return a string in the fromat HH:MM 
+	*/
+	public String toString()
+	{
+		return String.format("%2d:%2d",this.hours,this.minutes);
+	}
+}
