b02574b059290176464cbab4e47c8cc7b06e5413
Anupras Burokas <anuprasb750@gmail.com>
2021-04-27 18:11:22 -0600

    changes

--- src/main/java/com/uregina/app/Ticket.java
+++ src/main/java/com/uregina/app/Ticket.java
@@ -60,12 +60,7 @@ public class Ticket
 		//condition 4: max layover time >= total layover times combined
 		int layoverTime = 0;
 		for (int i=0; i < ticket.size() - 1; i++){
-			//layoverTime += Flight.calculateLayoverTime(ticket.get(i), ticket.get(i+1));
-			try {
-				layoverTime += Flight.calculateLayoverTime(ticket.get(i), ticket.get(i+1));
-			} catch (MoreThanOneDayException e) {
-				e.printStackTrace();
-			}
+			layoverTime += Flight.calculateLayoverTime(ticket.get(i), ticket.get(i+1));
 		}
 
 		if (layoverTime > maxLayoverTime) return false;
